33adf9aa2c17682a5c2a7edf6bd4fcf36ba270f7
bot-axl@accelnomics.com <axlbot@accelnomics.com>
2024-03-15 16:54:10 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import 'colors';
+
+const PORT = process.env.PORT || 3002;
+const app = express();
+const corsOption = { origin: 'http://localhost:3000' };
+app.use(cors(corsOption));
+
+
+app.get('/test', (req, res) => {
+  res.send('RESPONSE FROM ENTRANCE GRANTED');
+});
+
+app.get('/',(req,res) => {
+res.send('Welcome to Entrance Granted');
+});
+
+app.listen(PORT, () =>
+  console.log(
+    `Server is listening in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
+  )
+);
---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const [result, setResult] = useState('');
+  const PORT = process.env.REACT_APP_PORT || 3002;
+  const getUsers = async () => {
+    const data = await axios.get(`http://localhost:${PORT}/test`);
+    setResult(data?.data);
+  };
+
+  useEffect(() => {
+    getUsers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className='App'>
+      <header className='App-header'>Entrance Granted</header>
+      <section>
+        <h1>{result}</h1>
+      </section>
+    </div>
+  );
+}
+
+export default App;
