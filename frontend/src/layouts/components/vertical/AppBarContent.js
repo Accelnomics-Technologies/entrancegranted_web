e426502cdec03724e53faf04ec1f5ad44dafbf04
mookusamy.n <mookusamy.n@accelnomics.com>
2024-03-21 17:42:01 +0530

    dashboard web and mobile versions are updated

--- frontend/src/layouts/components/vertical/AppBarContent.js
+++ frontend/src/layouts/components/vertical/AppBarContent.js
@@ -9,6 +9,17 @@ import Icon from 'src/@core/components/icon'
 import ModeToggler from 'src/@core/layouts/components/shared-components/ModeToggler'
 import UserDropdown from 'src/@core/layouts/components/shared-components/UserDropdown'
 
+import { styled, useTheme } from '@mui/material/styles'
+import Link from 'next/link'
+
+const LinkStyled = styled(Link)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  textDecoration: 'none',
+  marginRight: theme.spacing(8)
+}))
+import themeConfig from 'src/configs/themeConfig'
+
 const AppBarContent = props => {
   // ** Props
   const { hidden, settings, saveSettings, toggleNavVisibility } = props
@@ -17,10 +28,14 @@ const AppBarContent = props => {
     <Box sx={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
       <Box className='actions-left' sx={{ mr: 2, display: 'flex', alignItems: 'center' }}>
         {hidden ? (
-          <IconButton color='inherit' sx={{ ml: -2.75 }} onClick={toggleNavVisibility}>
-            <Icon fontSize='1.5rem' icon='tabler:menu-2' />
-          </IconButton>
-        ) : null}
+          <LinkStyled href='/' className='logo_header'>
+            <img src='../../../images/pages/Entrance-Granted-Logo.svg' />
+            {themeConfig.templateName}
+          </LinkStyled>
+        ) : //   <IconButton color='inherit' sx={{ ml: -2.75 }} onClick={toggleNavVisibility}>
+        //   <Icon fontSize='1.5rem' icon='tabler:menu-2' />
+        // </IconButton>
+        null}
 
         <ModeToggler settings={settings} saveSettings={saveSettings} />
       </Box>
